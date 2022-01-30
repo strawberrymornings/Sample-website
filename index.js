2f6681d72c0643c03950e3817737eb4934bd46df
strawberrymornings <91802031+strawberrymornings@users.noreply.github.com>
2022-01-30 16:53:16 -0500

    Add files via upload
    added "posts" and "mygithub" buttons to my homepage!

---
+++ index.js
@@ -0,0 +1,9 @@
+var posts = document.getElementById("posts");
+
+var mygithub = document.getElementById("mygithub");
+
+mygithub.addEventListener("click", goToGithub); 
+
+function goToGithub () {
+location.href = "https://github.com/strawberrymornings";
+}
