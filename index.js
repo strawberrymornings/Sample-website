111a5d6e975892ecbc234867522c292c7dbcb39d
strawberrymornings <91802031+strawberrymornings@users.noreply.github.com>
2022-01-31 12:12:30 -0500

    Add files via upload
    debugged 404 redirect issue

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ var posts = document.getElementById("posts");
 
 var mygithub = document.getElementById("mygithub");
 
+mygithub.addEventListener("click", goToGithub);
+
+function goToGithub () {
+window.open(
+'https://www.github.com/strawberrymornings',
+'_blank' //opens link in new tab
+)
+
+
+}
